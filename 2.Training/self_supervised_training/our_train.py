227a84852902d8192605a57f4c55b01b71168bdd
GioviDetto <giovi.detto2000@gmail.com>
2024-03-21 14:30:48 +0000

    update: working on context window - not working

--- 2.Training/self_supervised_training/our_train.py
+++ 2.Training/self_supervised_training/our_train.py
@@ -1,11 +1,20 @@
 import sys  # nopep8
 
 sys.path.append("../")  # nopep8
-from core.functions.option_parser import get_training_options
-from core.classes.logger import TrainingExperimentLogger
-from core.classes.dataset_for_self_supervision import UnixDataMaskedLanguage
-from core.classes.mlm_model import MaskedLogModel
-from core.classes.tokenizer import LogPrecisTokenizer
+
+try:
+    from core.functions.option_parser import get_training_options
+    from core.classes.logger import TrainingExperimentLogger
+    from core.classes.dataset_for_self_supervision import UnixDataMaskedLanguage
+    from core.classes.mlm_model import MaskedLogModel
+    from core.classes.tokenizer import LogPrecisTokenizer
+except ImportError:
+    sys.path.append("2.Training")
+    from core.functions.option_parser import get_training_options
+    from core.classes.logger import TrainingExperimentLogger
+    from core.classes.dataset_for_self_supervision import UnixDataMaskedLanguage
+    from core.classes.mlm_model import MaskedLogModel
+    from core.classes.tokenizer import LogPrecisTokenizer
 
 
 def run(opts):
@@ -39,4 +48,35 @@ def run(opts):
 
 
 if __name__ == "__main__":
-    run(get_training_options())
+    dict_input = {
+        "identifier": "Your_self_supervised_eperiment_ID",
+        "task": "self_supervision",
+        "entity": "token",
+        "seed": 1,
+        "log_level": "info",
+        "no_cuda": False,
+        "output_path": "results/",
+        "use_date": False,
+        "clean_start": True,
+        "input_data": "../../1.Dataset/Training/Self_supervised/training_set.csv",
+        "validation_path": "../../1.Dataset/Training/Self_supervised/validation_set.csv",
+        "eval_size": 0.2,
+        "truncation": "default",
+        "available_percentage": 1.0,
+        "model_name": "microsoft/codebert-base",
+        "finetuned_path": "",
+        "tokenizer_name": "microsoft/codebert-base",
+        "special_token": "[STAT]",
+        "max_chunk_length": 256,
+        "epochs": 1,
+        "lr": 5e-06,
+        "patience": 4,
+        "observed_val_metric": "loss",
+        "batch_size": 16,
+        "mlm_probability": 0.15,
+        "use_cuda": False,
+        "run_name": "Your_self_supervised_eperiment_ID",
+        "training_stage": "training",
+    }
+    run(dict_input)
+    # run(get_training_options())

24ffbe5e2af3b9cb2ce73dd0d31ce0176927d369
AlessandroRedi <alessandroredi1@gmail.com>
2024-04-02 08:38:48 +0200

    new requirements

--- 2.Training/supervised_training/train.sh
+++ 2.Training/supervised_training/train.sh
@@ -1,30 +1,34 @@
 SEED=(5 102 118 130 777)
 
+#Experiment details
+TASK="entity_classification"
+DEVICES=0 #if running on cpu, add --no_cuda below
+LOG_LEVEL="info"
+OUTPUT_PATH="./results/"
+
+#Model info
+MODEL_NAME="ehsanaghaei/SecureBERT" #Chosen model
+FINETUNED_PATH="ehsanaghaei/SecureBERT" #Path, on your filesystem, to the finetuned model (e.g., if any domain-adapted) or online models
+TOKENIZER_NAME="ehsanaghaei/SecureBERT" #if you use a finetuned tokenizer, specify the path 
+MAX_CHUNK_LENGTH=512
+
+#Training details
+BATCH_SIZE=8
+EPOCHS=50 #Training epochs
+TRUNCATION="context_chunking" #"default", "simple_chunking", "context_chunking"
+ENTITY="token"
+AVAILABLE_PERCENTAGE=1
+
 for j in 0 1 2 3 4
 do
     #Experiment details
-    EXPERIMENT_IDENTIFIER=("secureBERT_LR_00005" "secureBERT_LR_00001" "secureBERT_LR_000005" "secureBERT_LR_000001") 
-    TASK="entity_classification"
-    DEVICES=0 #if running on cpu, add --no_cuda below
-    LOG_LEVEL="info"
-    OUTPUT_PATH="./results/"
+    EXPERIMENT_IDENTIFIER=("secureBERT_no_DA_LR_00005" "secureBERT_no_DA_LR_00001" "secureBERT_no_DA_LR_000005" "secureBERT_no_DA_LR_000001") 
 
     #Input info
     INPUT_DATA="../../1.Dataset/Training/Supervised/Partition/${SEED[j]}/sample_train_corpus.parquet"
-    
-    #Model info
-    MODEL_NAME="ehsanaghaei/SecureBERT" #Chosen model
-    FINETUNED_PATH="../../2.Training/self_supervised_training/results/self_supervision/token/ehsanaghaei_SecureBERT/secureBERT_LR_00005/seed_1/best_model" #Path, on your filesystem, to the finetuned model (e.g., if any domain-adapted) or online models
-    TOKENIZER_NAME="ehsanaghaei/SecureBERT" #if you use a finetuned tokenizer, specify the path 
-    MAX_CHUNK_LENGTH=512
 
     #Training details
-    BATCH_SIZE=8
-    EPOCHS=50 #Training epochs
     LR=(0.00005 0.00001 0.000005 0.000001) #Or smaller, 0.000001
-    TRUNCATION="context_chunking" #"default", "simple_chunking", "context_chunking"
-    ENTITY="token"
-    AVAILABLE_PERCENTAGE=1
 
     for i in 0 1 2 3
     do

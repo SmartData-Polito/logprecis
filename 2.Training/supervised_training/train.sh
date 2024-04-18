7dd757f5bb5c6e265ee9e79cbd10827a3a7d8111
GioviDetto <giovi.detto2000@gmail.com>
2024-04-18 13:06:46 +0200

    update: correction of metrics computation

--- 2.Training/supervised_training/train.sh
+++ 2.Training/supervised_training/train.sh
@@ -1,5 +1,5 @@
 #Experiment details
-EXPERIMENT_IDENTIFIER="reproduce_logprecis" 
+EXPERIMENT_IDENTIFIER="reproduce_logprecis_2" 
 TASK="entity_classification"
 DEVICES=0 #if running on cpu, add --no_cuda below
 LOG_LEVEL="info"
@@ -12,7 +12,7 @@ EVAL_SIZE=0.0
 
 #Model info
 MODEL_NAME="microsoft/codebert-base" #Chosen model
-FINETUNED_PATH="SmartDataPolito/SecureShellBert" #Path, on your filesystem, to the finetuned model (e.g., if any domain-adapted) or online models
+FINETUNED_PATH="../../2.Training/self_supervised_training/results/self_supervision/token/microsoft_codebert-base/baseline_codeBERT_0.00005/seed_1/best_model" #Path, on your filesystem, to the finetuned model (e.g., if any domain-adapted) or online models
 TOKENIZER_NAME="microsoft/codebert-base" #if you use a finetuned tokenizer, specify the path 
 MAX_CHUNK_LENGTH=512
 
@@ -23,10 +23,11 @@ LR=0.000001 #Or smaller, 0.000001
 TRUNCATION="context_chunking" #"default", "simple_chunking", "context_chunking"
 ENTITY="token"
 AVAILABLE_PERCENTAGE=1
+METRIC="f1"
 
 CUDA_VISIBLE_DEVICES="$DEVICES" python ./train.py --identifier "$EXPERIMENT_IDENTIFIER" --task "$TASK" \
     --model_name "$MODEL_NAME" --finetuned_path "$FINETUNED_PATH" --tokenizer_name "$TOKENIZER_NAME"  \
     --clean_start --truncation "$TRUNCATION" --log_level "$LOG_LEVEL" --output_path "$OUTPUT_PATH" \
     --entity "$ENTITY" --input_data "$INPUT_FOLDER" --available_percentage "$AVAILABLE_PERCENTAGE" \
     --epochs "$EPOCHS" --seed "$SEED" --batch_size "$BATCH_SIZE" --lr "$LR" \
-    --eval_size "$EVAL_SIZE" --max_chunk_length "$MAX_CHUNK_LENGTH"
+    --eval_size "$EVAL_SIZE" --max_chunk_length "$MAX_CHUNK_LENGTH" --observed_val_metric "$METRIC"

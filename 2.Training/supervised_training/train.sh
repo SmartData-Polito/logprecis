f01ce55e929bcfa66102bd95a9d7828ec51c0a81
AlessandroRedi <alessandroredi1@gmail.com>
2024-06-04 17:15:39 +0200

    Adjustment to main files

--- 2.Training/supervised_training/train.sh
+++ 2.Training/supervised_training/train.sh
@@ -1,43 +1,32 @@
-SEED=(5 102 118 130 777)
-
 #Experiment details
+EXPERIMENT_IDENTIFIER="reproduce_logprecis" 
 TASK="entity_classification"
 DEVICES=0 #if running on cpu, add --no_cuda below
 LOG_LEVEL="info"
 OUTPUT_PATH="./results/"
+SEED=1
+
+#Input info
+INPUT_FOLDER="../../1.Dataset/Training/Supervised/full_supervised_corpus.json"
+EVAL_SIZE=0.0 
 
 #Model info
-MODEL_NAME="ehsanaghaei/SecureBERT" #Chosen model
-FINETUNED_PATH="../../2.Training/self_supervised_training/results/self_supervision/token/ehsanaghaei_SecureBERT/secureBERT_LR_00005/seed_1/best_model" #Path, on your filesystem, to the finetuned model (e.g., if any domain-adapted) or online models
-TOKENIZER_NAME="ehsanaghaei/SecureBERT" #if you use a finetuned tokenizer, specify the path 
+MODEL_NAME="microsoft/codebert-base" #Chosen model
+FINETUNED_PATH="SmartDataPolito/SecureShellBert" #Path, on your filesystem, to the finetuned model (e.g., if any domain-adapted) or online models
+TOKENIZER_NAME="microsoft/codebert-base" #if you use a finetuned tokenizer, specify the path 
 MAX_CHUNK_LENGTH=512
 
 #Training details
 BATCH_SIZE=8
-EPOCHS=100 #Training epochs
+EPOCHS=47 #Training epochs
+LR=0.000001 #Or smaller, 0.000001
 TRUNCATION="context_chunking" #"default", "simple_chunking", "context_chunking"
 ENTITY="token"
 AVAILABLE_PERCENTAGE=1
-METRIC="loss"
 
-for j in 0 1 2 3 4
-do
-    #Training details
-    LR=(0.000001) #Or smaller, 0.000001
-
-    #Input info
-    INPUT_DATA="../../1.Dataset/Training/Supervised/Partition/${SEED[j]}/sample_train_corpus.parquet"
-
-    for i in 0
-    do
-        #Experiment details
-        EXPERIMENT_IDENTIFIER="secureBERT_LR_${LR[i]}_long_epoch" 
-    
-        CUDA_VISIBLE_DEVICES="$DEVICES" python ./train.py --identifier "$EXPERIMENT_IDENTIFIER" --task "$TASK" \
-            --model_name "$MODEL_NAME" --finetuned_path "$FINETUNED_PATH" --tokenizer_name "$TOKENIZER_NAME"  \
-            --clean_start --truncation "$TRUNCATION" --log_level "$LOG_LEVEL" --output_path "$OUTPUT_PATH" \
-            --entity "$ENTITY" --input_data "$INPUT_DATA" --available_percentage "$AVAILABLE_PERCENTAGE" \
-            --epochs "$EPOCHS" --seed "${SEED[j]}" --batch_size "$BATCH_SIZE" --lr "${LR[i]}" \
-            --max_chunk_length "$MAX_CHUNK_LENGTH" --observed_val_metric "$METRIC"
-    done
-done
+CUDA_VISIBLE_DEVICES="$DEVICES" python ./train.py --identifier "$EXPERIMENT_IDENTIFIER" --task "$TASK" \
+    --model_name "$MODEL_NAME" --finetuned_path "$FINETUNED_PATH" --tokenizer_name "$TOKENIZER_NAME"  \
+    --clean_start --truncation "$TRUNCATION" --log_level "$LOG_LEVEL" --output_path "$OUTPUT_PATH" \
+    --entity "$ENTITY" --input_data "$INPUT_FOLDER" --available_percentage "$AVAILABLE_PERCENTAGE" \
+    --epochs "$EPOCHS" --seed "$SEED" --batch_size "$BATCH_SIZE" --lr "$LR" \
+    --eval_size "$EVAL_SIZE" --max_chunk_length "$MAX_CHUNK_LENGTH"

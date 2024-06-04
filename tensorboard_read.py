db63841a13b62a5f54eda3adceacb0ccfb066eff
AlessandroRedi <alessandroredi1@gmail.com>
2024-06-04 17:06:24 +0200

    Removed useless files

--- tensorboard_read.py
+++
@@ -1,7 +0,0 @@
-from tbparse import SummaryReader
-log_dir = "2.Training/supervised_training/results/entity_classification/token/ehsanaghaei_SecureBERT/secureBERT_LR_000005/seed_777"
-reader = SummaryReader(log_dir)
-df = reader.text
-# print(df)
-df.to_markdown("text777.md")
-# df.to_csv("Scalars.csv")
